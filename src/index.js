3ec4a1b472834faf89ce1c2ff75a9b7546b4eb5c
Marek Łukasiewicz <marqus34@gmail.com>
2020-04-24 17:27:12 +0200

    Add product loader

--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ import './styles/bulmasetup.sass'
 import './styles/style.sass'
 
 window.addEventListener('DOMContentLoaded', () => {
-  console.log('page loaded')
-
   import('./modules/pageutils.js').then(module => {
     module.addButtonScroll()
+    module.addProductLoader()
   })
-
 })
--- src/modules/pageutils.js
+++ src/modules/pageutils.js
@@ -48,3 +48,53 @@ export function addButtonScroll() {
     }
   })
 }
+
+function mockApiCall() {
+  let item = {
+    name: 'Dynamically loaded item',
+    tags: 'SO DYNAMIC',
+    price: '$ 99.70',
+    image: 'https://via.placeholder.com/309x390'
+  }
+  let collection = []
+  for (let i = 0; i < 4; i++) collection.push(item)
+  return collection
+}
+
+function loadProducts(containerRef) {
+  let responseData = mockApiCall()
+
+  for (const item of responseData) {
+    // I knew from Vue that templates are cool and I will not pretend this is not copied from:
+    // https://stackoverflow.com/questions/494143/creating-a-new-dom-element-from-an-html-string-using-built-in-dom-methods-or-pro/35385518#35385518
+    let template = document.createElement('template');
+    let html = `
+      <div class="column is-one-quarter-desktop is-one-third-tablet is-half-mobile">
+        <div class="is-inline-block has-text-left">
+          <img src="${item.image}" alt="Photo of ${item.name}">
+          <p>${item.tags}</p>
+          <p>${item.name}</p>
+          <p>${item.price}</p>
+        </div>
+      </div>
+    `
+    html = html.trim();
+    template.innerHTML = html;
+    containerRef.appendChild(template.content.firstChild)
+  }
+}
+
+export function addProductLoader() {
+  var buttonProducts = document.getElementById('button-all-products')
+  if(buttonProducts === null) {
+    console.log('Cannot mount ProductLoader without element: #button-all-products')
+    return
+  }
+  var containerProducts = document.getElementById('container-all-products')
+  if(containerProducts === null) {
+    console.log('Cannot mount ProductLoader without element: #container-all-products')
+    return
+  }
+
+  buttonProducts.addEventListener('click', () => {loadProducts(containerProducts)})
+}
